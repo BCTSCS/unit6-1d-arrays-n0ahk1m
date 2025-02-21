f18e1db5681af26caba81b19fe47e1ff2fb1b276
n0ahk1m <noahmatiaskim@gmail.com>
2025-02-21 14:09:35 -0500

    working on stuff

--- DataAnalyzer.java
+++ DataAnalyzer.java
@@ -1,12 +1,157 @@
-import java.io.File;
-import java.io.IOException;
-import java.util.Scanner;
 
 public class DataAnalyzer{
 
-    public static int searchList(int target, int[] numbers){
+    public static String findCountryByInternetUsage(double internet, double[] internetList, String[] countryList){
+        for (int i = 0; i < internetList.length; i++){
+            if (internet == internetList[i]){
+                return countryList[i];
+            }
+        }
 
-        return -1;
+        return "None";
+    }
+
+    public static String findCountryByIncomeLevel(String incomeLevel, String[] incomeList, String[] countryList){
+        for (int i = 0; i < incomeList.length; i++){
+            if (incomeLevel.equals(incomeList[i])){
+                return countryList[i];
+            }
+        }
+
+        return "None";
+    }
+
+    public static String getMinInternetCountry(String[] countries, double[] internetPercents){
+        String country = countries[0];
+        double minInternet = internetPercents[0];
+
+        for (int i = 0; i < internetPercents.length; i++){
+            if (internetPercents[i] < minInternet){
+                minInternet = internetPercents[i];
+                country = countries[i];
+            }
+        }
+
+        return country;
+    }
+
+    public static String getMaxInternetCountry(String[] countries, double[] internetPercents){
+        String country = countries[0];
+        double maxInternet = internetPercents[0];
+
+        for (int i = 0; i < internetPercents.length; i++){
+            if (internetPercents[i] > maxInternet){
+                maxInternet = internetPercents[i];
+                country = countries[i];
+            }
+        }
+
+        return country;
+    }
+
+    public static double getAverageInternet(double[] internetPercents){
+        double total = 0.0;
+        for (double internet : internetPercents){
+            total += internet;
+        }
+
+        double average = total/internetPercents.length;
+
+        return average;
+    }
+
+    public static double getModeInternet(double[] internetPercents){
+        double mode = internetPercents[0];
+        int maxCount = 1;
+        for (int i = 0; i < internetPercents.length; i++){
+            int count = 0;
+            for (int j = 0; j < internetPercents.length; j++){
+                if (internetPercents[j] == internetPercents[i]){
+                    count++;
+                }
+            }
+
+            if (count > maxCount){
+                maxCount = count;
+                mode = internetPercents[i];
+            }
+        }
+
+        return mode;
+    }
+
+    public static double getMedianInternet(double[] internetPercents){
+        double[] sortedInternet = ascendingDoubleSort(internetPercents);
+
+        double median;
+        if (sortedInternet.length%2 == 0){
+            median = (sortedInternet[sortedInternet.length/2] + sortedInternet[sortedInternet.length/2 - 1]) / 2;
+        }
+        else {
+            median = sortedInternet[sortedInternet.length/2];
+        }
+
+        return median;
+    }
+
+    public static String[] findLowestInternetCountries(int numCountries, double[] internetPercents, String[] countries){
+        String[] finalCountries = new String[numCountries];
+        int[] finalCountriesIndexes = new int[numCountries];
+
+        for (int i = 0; i < numCountries; i++){
+            finalCountriesIndexes[i] = i;
+        }
+
+        for (int i = numCountries; i < internetPercents.length; i++){
+            for (int j = 0; j < numCountries; j++){
+                if (internetPercents[i] < internetPercents[finalCountriesIndexes[j]]){
+                    finalCountriesIndexes[j] = i;
+                    break;
+                }
+            }
+        }
+
+        for (int i = 0; i < numCountries; i++){
+            finalCountries[i] = countries[finalCountriesIndexes[i]];
+        }
+
+        return finalCountries;
+    }
+
+    public static int[] ascendingIntSort(int[] arr){
+        for (int i = 0; i < arr.length - 1; i++){
+            int minIndex = i;
+
+            for (int j = i + 1; j < arr.length; j++){
+                if (arr[j] < arr[minIndex]){
+                    minIndex = j;
+                }
+            }
+
+            int tmp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = tmp;
+        }
+
+        return arr;
+    }
+
+    public static double[] ascendingDoubleSort(double[] arr){
+        for (int i = 0; i < arr.length - 1; i++){
+            int minIndex = i;
+
+            for (int j = i + 1; j < arr.length; j++){
+                if (arr[j] < arr[minIndex]){
+                    minIndex = j;
+                }
+            }
+
+            double tmp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = tmp;
+        }
+
+        return arr;
     }
 
     // Linear search
@@ -56,33 +201,51 @@ public class DataAnalyzer{
     }
 
     public static void main(String[] args){
-        int[] arr = {10,20,30,40,50};
-        System.out.println(searchList(arr,50));
-        System.out.println(binarySearch(arr,50));
-        int[] reversedArr = reverseList(arr);
-        for (int i = 0; i < reversedArr.length; i++){
-            System.out.print(reversedArr[i] + " ");
-        }
+        // int[] arr = {10,20,30,40,50};
+        // System.out.println(searchList(arr,50));
+        // System.out.println(binarySearch(arr,50));
+        // int[] reversedArr = reverseList(arr);
+        // for (int i = 0; i < reversedArr.length; i++){
+        //     System.out.print(reversedArr[i] + " ");
+        // }
 
-        System.out.println();
+        // System.out.println();
 
-        try{
-            File f = new File("numbers.txt");
-            Scanner input = new Scanner(f);
+        // try{
+        //     File f = new File("numbers.txt");
+        //     Scanner input = new Scanner(f);
 
-            int[] numbers = new int[100];
-            for (int i = 0; i < 100; i++){
-                numbers[i] = input.nextInt();
-            }
-            System.out.println(searchList(numbers,52));
-            System.out.println(binarySearch(numbers,0));
-            int[] reversedNumber = reverseList(numbers);
-            for (int i = 0; i < reversedNumber.length; i++){
-                System.out.print(reversedNumber[i] + " ");
-            }
-        }
-        catch(IOException e){
-            System.out.println("file not found");
-        }
+        //     int[] numbers = new int[100];
+        //     for (int i = 0; i < 100; i++){
+        //         numbers[i] = input.nextInt();
+        //     }
+        //     System.out.println(searchList(numbers,52));
+        //     System.out.println(binarySearch(numbers,0));
+        //     int[] reversedNumber = reverseList(numbers);
+        //     for (int i = 0; i < reversedNumber.length; i++){
+        //         System.out.print(reversedNumber[i] + " ");
+        //     }
+        // }
+        // catch(IOException e){
+        //     System.out.println("file not found");
+        // }
+
+        FileOperator countriesFile = new FileOperator("countries.txt");
+        FileOperator incomeFile = new FileOperator("incomes.txt");
+        FileOperator internetpercentFile = new FileOperator("internetpercent.txt");
+        FileOperator populationsFile = new FileOperator("populations.txt");
+        FileOperator unemploymentFile = new FileOperator("unemployment.txt");
+
+        String[] countries = countriesFile.toStringArray(215);
+        String[] incomes = incomeFile.toStringArray(215);
+        double[] internetPercents = internetpercentFile.toDoubleArray(215);
+        int[] populations = populationsFile.toIntArray(215);
+        double[] unemployment = unemploymentFile.toDoubleArray(215);
+
+        String filteredCountry;
+        filteredCountry = findCountryByInternetUsage(76.0, internetPercents, countries);
+        filteredCountry = findCountryByIncomeLevel("Low Income", incomes, countries);
+
+        System.out.println(filteredCountry);
     }
 }
